c2cb01bf8995ce2cb67b98d832bdf27c0a35cada
preboy.sl <preboy@126.com>
2018-03-16 16:38:04 +0800

    udate web/sever.js

--- sl/http_task/server.js
+++ sl/http_task/server.js
@@ -1,30 +1,36 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
+const fs = require('fs');
 
 let commands = []
 
-
-app.get('/task', function (req, res) {
+app.get('/task', function(req, res) {
     let str = commands.pop();
-    if (str == undefined){
+    if (str == undefined) {
         res.send("none");
+    } else {
+        // console.log("task:\t\t", str, commands.length);
+        res.send(str);
+        let d = new Date();
+        let log = `DO COMMAND: '${str}' AT ${d}\n`
+        fs.writeFileSync("web.log", log, {
+            encoding: "utf8",
+            flag: "a"
+        });
     }
-    // console.log("task:\t\t", str, commands.length);
-    res.send(str);
 });
 
 
-app.get('/push', function (req, res) {
+app.get('/push', function(req, res) {
     let str = req.query.command;
     commands.push(str)
-    // console.log("push:\t\t", str, commands.length);
+        // console.log("push:\t\t", str, commands.length);
     res.send('OK');
 });
 
 
-
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
